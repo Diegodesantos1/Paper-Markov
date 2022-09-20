90350dda1a68f1f765b0d91ac4302f8485fa8a69
jmedina28 <juanperezmedina67@gmail.com>
2022-09-20 10:30:48 +0200

    modificaciones

--- algoritmo.py
+++ algoritmo.py
@@ -2,17 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 # leemos el archivo csv
-def leerArchivo():
-    df = pd.read_csv('police_deaths.csv')
+def leerArchivo(csv):
+    df = pd.read_csv(csv)
     return df
 
-def calculoMediaAritmetica(caracteristica):
-    n = caracteristica.count()
-    sumaValoresObservaciones = 0
-    mediaAritmetica = 0
-    for valorObservacion in caracteristica:
-        sumaValoresObservaciones = sumaValoresObservaciones + valorObservacion
-    mediaAritmetica = sumaValoresObservaciones / n
-    return mediaAritmetica
-print(calculoMediaAritmetica(leerArchivo()["Tot Deaths/1M pop"]))
+print(leerArchivo('police_deaths.csv'))
+df = leerArchivo('police_deaths.csv')
+frecuencia = df.groupby(['Year']).count()
+print(frecuencia)
+
 
