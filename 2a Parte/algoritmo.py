8c32fb849ebcb7c8e36d19b954a05a63e6770c1e
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-09-21 14:18:49 +0200

    explicaciones

--- 2a Parte/algoritmo.py
+++ 2a Parte/algoritmo.py
@@ -28,72 +28,73 @@ def muertesPorAño(df):
 
 #print(muertesPorAño(df2))
 
+class Markov: #clase que calcula la probabilidad de que ocurra una secuencia determinada por probabilidades condicionadas
+    tiempos = leerArchivo('2a Parte/seattle-weather.csv')['weather'].tolist()
+    del(tiempos[0])
+    tiempo = []
+    for causa in tiempos:
+        if causa not in tiempo:
+            tiempo.append(causa)
+    print(tiempo)
 
-tiempos = leerArchivo('2a Parte/seattle-weather.csv')['weather'].tolist()
-del(tiempos[0])
-tiempo = []
-for causa in tiempos:
-    if causa not in tiempo:
-        tiempo.append(causa)
-print(tiempo)
+    conteo=Counter(tiempos)
 
-conteo=Counter(tiempos)
+    dic_prob_tiempo={}
+    for clave in conteo:  
+        valor=conteo[clave]
+        if valor != 1:
+            dic_prob_tiempo[clave] = valor
+    print(dic_prob_tiempo)
+    total = len(tiempos)
+    prob_sol = dic_prob_tiempo['sun']/total
+    prob_llu = dic_prob_tiempo['rain']/total
+    prob_niev = dic_prob_tiempo['snow']/total
+    #matriz probailiades condicionadas
+    m_t = [[0.6, 0.2, 0.1], [0.5, 0.4, 0.1], [0.7, 0.2, 0.1]]
+    #definimos las probabilidades relacionadas(definen el modelo de markov)
+    v_s = 0.1
+    nv_s = 0.9
+    v_ll = 0.7
+    nv_ll = 0.3
+    v_n = 0.4
+    nv_n = 0.6
 
-dic_prob_tiempo={}
-for clave in conteo:  
-   valor=conteo[clave]
-   if valor != 1:
-    dic_prob_tiempo[clave] = valor
-print(dic_prob_tiempo)
-total = len(tiempos)
-prob_sol = dic_prob_tiempo['sun']/total
-prob_llu = dic_prob_tiempo['rain']/total
-prob_niev = dic_prob_tiempo['snow']/total
-#matriz probailiades condicionadas
-m_t = [[0.6, 0.2, 0.1], [0.5, 0.4, 0.1], [0.7, 0.2, 0.1]]
-#definimos las probabilidades relacionadas(definen el modelo de markov)
-v_s = 0.1
-nv_s = 0.9
-v_ll = 0.7
-nv_ll = 0.3
-v_n = 0.4
-nv_n = 0.6
-
-#para poder elegir el valor correspondiente en la formula markov
-def sacar_valor(letra):
-    if letra == 'v':
-        return [v_s, v_ll, v_n]
-    else:
-        return [nv_s, nv_ll, nv_n]
-
-#probabilidad de una secuencia de estados(los definidos aleatoria)
-def markov(secuencia):
-    lista1 = []
-    lista2 = []
-    lista3 = []
-    listadefi = []
-    contador = 0
-    for i in range(len(secuencia)):
-        if contador == 0:
-            a1 = sacar_valor(secuencia[contador])[0]*prob_sol
-            a2 = sacar_valor(secuencia[contador])[1]*prob_llu
-            a3 = sacar_valor(secuencia[contador])[2]*prob_niev
-            lista1.append(a1)
-            lista2.append(a2)
-            lista3.append(a3)
+    #para poder elegir el valor correspondiente en la formula markov
+    def sacar_valor(letra):
+        if letra == 'v':
+            return [Markov.v_s, Markov.v_ll, Markov.v_n]
         else:
-            a1 = sacar_valor(secuencia[contador])[0]*(m_t[0][0]*lista1[contador-1]+m_t[1][0]*lista2[contador-1]+m_t[2][0]*lista3[contador-1])
-            a2 = sacar_valor(secuencia[contador])[1]*(m_t[0][1]*lista1[contador-1]+m_t[1][1]*lista2[contador-1]+m_t[2][1]*lista3[contador-1])
-            a3 = sacar_valor(secuencia[contador])[2]*(m_t[0][2]*lista1[contador-1]+m_t[1][2]*lista2[contador-1]+m_t[2][2]*lista3[contador-1])
-            lista1.append(a1)
-            lista2.append(a2)
-            lista3.append(a3)
-        contador+=1
-    listadefi.append(lista1[-1])
-    listadefi.append(lista2[-1])
-    listadefi.append(lista3[-1])
-    return listadefi
+            return [Markov.nv_s, Markov.nv_ll, Markov.nv_n]
 
+    #probabilidad de una secuencia de estados(los definidos aleatoria)
+    def markov(secuencia):
+        lista1 = []
+        lista2 = []
+        lista3 = []
+        listadefi = []
+        contador = 0
+        for i in range(len(secuencia)):
+            if contador == 0:
+                a1 = Markov.sacar_valor(secuencia[contador])[0]*Markov.prob_sol
+                a2 = Markov.sacar_valor(secuencia[contador])[1]*Markov.prob_llu
+                a3 = Markov.sacar_valor(secuencia[contador])[2]*Markov.prob_niev
+                lista1.append(a1)
+                lista2.append(a2)
+                lista3.append(a3)
+            else:
+                a1 = Markov.sacar_valor(secuencia[contador])[0]*(Markov.m_t[0][0]*lista1[contador-1]+Markov.m_t[1][0]*lista2[contador-1]+Markov.m_t[2][0]*lista3[contador-1])
+                a2 = Markov.sacar_valor(secuencia[contador])[1]*(Markov.m_t[0][1]*lista1[contador-1]+Markov.m_t[1][1]*lista2[contador-1]+Markov.m_t[2][1]*lista3[contador-1])
+                a3 = Markov.sacar_valor(secuencia[contador])[2]*(Markov.m_t[0][2]*lista1[contador-1]+Markov.m_t[1][2]*lista2[contador-1]+Markov.m_t[2][2]*lista3[contador-1])
+                lista1.append(a1)
+                lista2.append(a2)
+                lista3.append(a3)
+            contador+=1
+        listadefi.append(lista1[-1])
+        listadefi.append(lista2[-1])
+        listadefi.append(lista3[-1])
+        return listadefi
+
+#Comprobamos con una secuencia cualquiera de nuestro markov si funciona
 listadeprueba = ['v', 'nv', 'v', 'v', 'v', 'nv'] #Para probar si funciona markov
-lista_probs = markov(['v', 'nv', 'v', 'v', 'v', 'nv'])
+lista_probs = Markov.markov(['v', 'nv', 'v', 'v', 'v', 'nv'])
 print("La probabilidad de que salga una secuencia del tipo "+str(listadeprueba)+" es de "+str((lista_probs[0]+lista_probs[1]+lista_probs[2])))
