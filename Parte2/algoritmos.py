a48d1a4cf19b0972a204589bb9b3ac7cb80c29d7
jmedina28 <juanperezmedina67@gmail.com>
2022-10-01 19:32:11 +0200

    recoloco la matriz para entender bien lo que hago

--- Parte2/algoritmos.py
+++ Parte2/algoritmos.py
@@ -7,7 +7,7 @@ def dtw(a, b):
     n, m = len(a), len(b) # obtengo n y m que son las dimensiones de las listas introducidas
     matrix = np.zeros((n+1, m+1)) # creo matriz de ceros
     matrix[0, 1:], matrix[1:, 0] = np.inf, np.inf # establezco los infinitos
-
+    matrix = matrix[::-1] # dejo colocada como me interesa personalmente la matriz
 
     print(matrix) # imprimo la matriz para ver lo que devuelve
 
