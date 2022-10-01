eaaa1740490fadeea9737e70be496c0ea12896f5
jmedina28 <juanperezmedina67@gmail.com>
2022-10-01 19:50:57 +0200

    añado documentacion y explicacion del código

--- Parte2/algoritmos.py
+++ Parte2/algoritmos.py
@@ -9,10 +9,14 @@ def dtw(a, b):
     matrix = np.zeros((n+1, m+1)) # creo matriz de ceros
     matrix[0, 1:], matrix[1:, 0] = np.inf, np.inf # establezco los infinitos
     
+    """Para generar mi matriz y obtener los elementos me he basado en la explicación del algoritmo del siguiente video:
+    https://www.youtube.com/watch?v=9GdbMc4CEhE
+    """
+
     for i in range(1,n+1): # recorro filas
         for j in range(1,m+1): # recorro columnas
-            list_values = [matrix[i-1][j-1],matrix[i-1][j],matrix[i][j-1]]
-            matrix[i][j] = abs(a[i-1]-b[j-1]) + min(list_values)
+            list_values = [matrix[i-1][j-1],matrix[i-1][j],matrix[i][j-1]] # obtengo una lista con los valores minimos que me interesan
+            matrix[i][j] = abs(a[i-1]-b[j-1]) + min(list_values) # calculo la distancia entre a(i) y b(j) y le sumo el valor minimo de la lista
     
     matrix = matrix[::-1] # dejo colocada como me interesa personalmente la matriz para poder verla bien
 
