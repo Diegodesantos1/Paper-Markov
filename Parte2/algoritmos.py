d9a3a95867aa9307904a3c0f114d9606bda6a80f
jmedina28 <juanperezmedina67@gmail.com>
2022-10-01 19:24:26 +0200

    dejo ya creada la estructura básica y principal de la matrix

--- Parte2/algoritmos.py
+++ Parte2/algoritmos.py
@@ -6,6 +6,8 @@ lista2 = [3,1,1,3]
 def dtw(a, b):
     n, m = len(a), len(b)
     matrix = np.zeros((n+1, m+1))
+    matrix[0, 1:], matrix[1:, 0] = np.inf, np.inf
+
     print(matrix)
 
 dtw(lista1, lista2)
