58a2c934d93f6bfbbfdd25abc953cca53dd71c35
jmedina28 <juanperezmedina67@gmail.com>
2022-10-01 19:42:15 +0200

    lo que hago con este bucle es sustituir todos los elementos de la matriz por el que me interese, esto solo lo uso para estructurar el codigo que voy a hacer ahora

--- Parte2/algoritmos.py
+++ Parte2/algoritmos.py
@@ -9,6 +9,10 @@ def dtw(a, b):
     matrix = np.zeros((n+1, m+1)) # creo matriz de ceros
     matrix[0, 1:], matrix[1:, 0] = np.inf, np.inf # establezco los infinitos
     
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            matrix[i][j] = 5
+    
     matrix = matrix[::-1] # dejo colocada como me interesa personalmente la matriz para poder verla bien
 
     print(matrix), print("matriz nxm") # imprimo la matriz para ver lo que devuelve
